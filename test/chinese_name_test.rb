5ebf2f7169ecd67cdaaa0576703ea15a89ab79ce
Grayson Chen <cgg5207@sina.com>
2017-08-24 07:18:08 +0800

    add test case

--- test/chinese_name_test.rb
+++ test/chinese_name_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
-
-ENV["LAST_NAME_FILE"] = File.dirname(__FILE__) + '/last_name_test.dat'
+LAST_NAME_FILE = File.dirname(__FILE__) + '/last_name_test.dat'
 
 class ChineseNameTest < Minitest::Test
   def test_g_last_name
+    ENV["LAST_NAME_FILE"] = LAST_NAME_FILE
     assert_equal '陈', ChineseName.g.last_name
+  end
+
+  def test_g_all_last_name_count
+    ENV["LAST_NAME_FILE"] = nil
+    assert_equal 434, ChineseName.g.send('all_last_name').count
   end
 end
